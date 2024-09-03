56026ba953ff6895c46b4ab1fb0a1556bb47fc68
EBayego <edubayego@gmail.com>
2024-09-03 12:12:15 +0200

    Updated Llama

--- backend/server/inference.py
+++ backend/server/inference.py
@@ -1,10 +1,8 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from gradio_client import Client
 from .decrypt import ret_pass
 from sqlalchemy import create_engine, MetaData, Table, text, or_
 from sqlalchemy.sql import select
 
-# Connect with Hugging Face Spaces
-client = Client("EBayego/LlamaTest", hf_token="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")
+# Connect with Hugging Face Spaces llama only available for PRO account in HF
+#client = Client("EBayego/LlamaTest", hf_token="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")
